b16fd38b30fc736689cc16cc6ed2cb8dc51e7e98
Abhinav Adhikari <abhinavadhikari87@gmail.com>
2025-10-18 13:50:29 +0545

    Implmented format

--- code/main.c
+++ code/main.c
@@ -18,19 +18,20 @@ typedef double f64;
 enum { T_PLUS, T_MINUS, T_STAR, T_SLASH, T_INTEGER_LITERAL };
 
 struct token {
-  int token;
-  int intvalue;
+	int token;
+	int intvalue;
 };
 
-int main(int argc, char *argv[]) {
-  if (argc < 2) {
-    fprintf(stderr, "Please specify a \033[31minput\033[0m file\n");
-    return (-1);
-  }
+int main(int argc, char *argv[])
+{
+	if (argc < 2) {
+		fprintf(stderr, "Please specify a \033[31minput\033[0m file\n");
+		return (-1);
+	}
 
-  for (int i = 1; i < argc; i++) {
-    fprintf(stdout, "Provided input of %s\n", argv[i]);
-  }
+	for (int i = 1; i < argc; i++) {
+		fprintf(stdout, "Provided input of %s\n", argv[i]);
+	}
 
-  return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
